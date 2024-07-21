4e779cd2913f7c1832a17de1791c937f533a5401
MiroslavStefanovic <mstef@uns.ac.rs>
2024-07-21 19:07:40 +0200

    refactoring

--- contracts/System.sol
+++ contracts/System.sol
@@ -6,27 +6,12 @@ contract System {
     // slither-disable too-many-digits
     address public constant NATIVE_TRANSFER_PRECOMPILE =
         0x0000000000000000000000000000000000002020;
-    address public constant VALIDATOR_PKCHECK_PRECOMPILE =
-        0x0000000000000000000000000000000000002030;
-    address public constant ALLOWLIST_PRECOMPILE =
-        0x0200000000000000000000000000000000000004;
-    address public constant BLOCKLIST_PRECOMPILE =
-        0x0300000000000000000000000000000000000004;
     address public constant VALIDATOR_BLS_PRECOMPILE =
         0x0000000000000000000000000000000000002060;
 
-    // internal addrs
-    address public constant SYSTEM = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
-
     // pre-compiled gas consumption
     uint256 public constant NATIVE_TRANSFER_PRECOMPILE_GAS = 21000;
-    uint256 public constant VALIDATOR_PKCHECK_PRECOMPILE_GAS = 150000;
-    uint256 public constant READ_ADDRESSLIST_GAS = 5000;
     uint256 public constant VALIDATOR_BLS_PRECOMPILE_GAS = 50000;
-
-    // genesis contracts
-    address public constant NATIVE_TOKEN_CONTRACT =
-        0x0000000000000000000000000000000000001010;
 
     // slither-disable-next-line unused-state,naming-convention
     uint256[50] private __gap;
