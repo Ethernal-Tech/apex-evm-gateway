eaaacead7fc12fcc9d78a19550576b9beeb089ef
MiroslavStefanovic <mstef@uns.ac.rs>
2024-08-16 09:24:32 +0200

    config fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-};
-
-module.exports = {
-  networks: {
-    hardhat: {},
-    coston2: {
-      url: process.env.COSTON2_URL,
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 35000000000,
-    },
-  },
-  solidity: {
-    version: "0.8.24",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
 };
 
 export default config;
