15a7a730e46b27716b3836efcf1b579d25360dcd
MiroslavStefanovic <mstef@uns.ac.rs>
2024-09-04 20:17:38 +0200

    adding event when there is not enough value before revert

--- contracts/interfaces/IGatewayStructs.sol
+++ contracts/interfaces/IGatewayStructs.sol
@@ -39,6 +39,15 @@ interface IGatewayStructs {
         uint256 feeAmount,
         uint256 value
     );
+
+    event WithdrawInsufficientValue(
+        uint8 destinationChainId,
+        address sender,
+        ReceiverWithdraw[] receivers,
+        uint256 feeAmount,
+        uint256 value
+    );
+
     event TTLExpired(bytes data);
 
     error NotGateway();
