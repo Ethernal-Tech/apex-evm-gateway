f4cb8593f402eaccbd9e33c693bb70121208ef16
MiroslavStefanovic <mstef@uns.ac.rs>
2024-09-16 10:35:37 +0200

    cleanup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-switch-network";
-import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
