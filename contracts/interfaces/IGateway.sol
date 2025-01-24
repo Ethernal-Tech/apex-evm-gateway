b6ca5424551d802d1c19e70c0cf59828b8381621
Miroslav Stefanović <32701370+MiroslavStefanovic@users.noreply.github.com>
2025-01-24 18:11:27 +0100

    audit: APEX-476 IGateway and INativeTokenPredicate don’t contain all custom functions

--- contracts/interfaces/IGateway.sol
+++ contracts/interfaces/IGateway.sol
@@ -15,4 +15,10 @@ interface IGateway is IGatewayStructs {
         ReceiverWithdraw[] calldata _receivers,
         uint256 _feeAmount
     ) external payable;
+
+    function updateValidatorsChainData(
+        bytes calldata _signature,
+        uint256 _bitmap,
+        bytes calldata _data
+    ) external;
 }
