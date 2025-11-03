409f47cb018b98c653331558155c3291891882c1
MiroslavStefanovic <mstef@uns.ac.rs>
2025-11-03 18:42:53 +0100

    old test fix and refactoring

--- test/NativeTokenPredicate.test.ts
+++ test/NativeTokenPredicate.test.ts
@@ -1,62 +1,96 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { deployGatewayFixtures, impersonateAsContractAndMintFunds } from "./fixtures";
+import {
+  deployGatewayFixtures,
+  impersonateAsContractAndMintFunds,
+} from "./fixtures";
 
 describe("NativeTokenPredicate Contract", function () {
   it("SetDependencies should fail if Gateway or NetiveToken is Zero Address", async () => {
-    const { owner, gateway, nativeTokenWallet, nativeTokenPredicate } = await loadFixture(deployGatewayFixtures);
-
     await expect(
-      nativeTokenPredicate.connect(owner).setDependencies(ethers.ZeroAddress, nativeTokenWallet.target)
+      nativeTokenPredicate
+        .connect(owner)
+        .setDependencies(ethers.ZeroAddress, nativeTokenWallet.target)
     ).to.to.be.revertedWithCustomError(nativeTokenPredicate, "ZeroAddress");
 
     await expect(
-      nativeTokenPredicate.connect(owner).setDependencies(gateway.target, ethers.ZeroAddress)
+      nativeTokenPredicate
+        .connect(owner)
+        .setDependencies(gateway.target, ethers.ZeroAddress)
     ).to.to.be.revertedWithCustomError(nativeTokenPredicate, "ZeroAddress");
   });
 
   it("SetDependencies should faild if not called by owner", async () => {
-    const { receiver, gateway, nativeTokenWallet, nativeTokenPredicate } = await loadFixture(deployGatewayFixtures);
-
     await expect(
-      nativeTokenPredicate.connect(receiver).setDependencies(gateway.target, nativeTokenWallet.target)
-    ).to.be.revertedWithCustomError(nativeTokenPredicate, "OwnableUnauthorizedAccount");
+      nativeTokenPredicate
+        .connect(receiver)
+        .setDependencies(gateway.target, nativeTokenWallet.target)
+    ).to.be.revertedWithCustomError(
+      nativeTokenPredicate,
+      "OwnableUnauthorizedAccount"
+    );
   });
 
   it("SetDependencies and validate initialization", async () => {
-    const { owner, gateway, nativeTokenWallet, nativeTokenPredicate } = await loadFixture(deployGatewayFixtures);
-
-    await expect(nativeTokenPredicate.connect(owner).setDependencies(gateway.target, nativeTokenWallet.target)).to.not
-      .be.reverted;
+    await expect(
+      nativeTokenPredicate
+        .connect(owner)
+        .setDependencies(gateway.target, nativeTokenWallet.target)
+    ).to.not.be.reverted;
 
     expect(await nativeTokenPredicate.gateway()).to.equal(gateway.target);
-    expect(await nativeTokenPredicate.nativeTokenWallet()).to.equal(nativeTokenWallet.target);
+    expect(await nativeTokenPredicate.nativeTokenWallet()).to.equal(
+      nativeTokenWallet.target
+    );
   });
 
   it("Deposit should fail if not called by Gateway", async () => {
-    const { receiver, nativeTokenPredicate, data } = await loadFixture(deployGatewayFixtures);
-
     const address = ethers.Wallet.createRandom().address;
 
-    await expect(nativeTokenPredicate.connect(receiver).deposit(data, address)).to.be.revertedWithCustomError(
-      nativeTokenPredicate,
-      "NotGateway"
-    );
+    await expect(
+      nativeTokenPredicate.connect(receiver).deposit(data, address, 0)
+    ).to.be.revertedWithCustomError(nativeTokenPredicate, "NotGateway");
   });
 
   it("Deposit should fail if batch is already executed", async () => {
-    const { gateway, nativeTokenPredicate, data } = await loadFixture(deployGatewayFixtures);
-
     const address = ethers.Wallet.createRandom().address;
 
-    const gatewayContract = await impersonateAsContractAndMintFunds(await gateway.target);
+    const gatewayContract = await impersonateAsContractAndMintFunds(
+      await gateway.target
+    );
 
-    await nativeTokenPredicate.connect(gatewayContract).deposit(data, address);
+    await nativeTokenPredicate
+      .connect(gatewayContract)
+      .deposit(data, address, 0);
 
-    await expect(nativeTokenPredicate.connect(gatewayContract).deposit(data, address)).to.be.revertedWithCustomError(
+    await expect(
+      nativeTokenPredicate.connect(gatewayContract).deposit(data, address, 0)
+    ).to.be.revertedWithCustomError(
       nativeTokenPredicate,
       "BatchAlreadyExecuted"
     );
+  });
+
+  let owner: any;
+  let receiver: any;
+  let gateway: any;
+  let nativeTokenPredicate: any;
+  let nativeTokenWallet: any;
+  let validatorsc: any;
+  let receiverWithdraw: any;
+  let data: any;
+
+  beforeEach(async function () {
+    const fixture = await loadFixture(deployGatewayFixtures);
+
+    owner = fixture.owner;
+    receiver = fixture.receiver;
+    gateway = fixture.gateway;
+    nativeTokenPredicate = fixture.nativeTokenPredicate;
+    nativeTokenWallet = fixture.nativeTokenWallet;
+    validatorsc = fixture.validatorsc;
+    receiverWithdraw = fixture.receiverWithdraw;
+    data = fixture.data;
   });
 });
